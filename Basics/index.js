c1d9364bc8b68b2d1e79c7f924e6685b97437538
labhk <labh.k2003@gmail.com>
2023-06-04 21:54:10 +0530

    new

--- Basics/index.js
+++ Basics/index.js
@@ -1,14 +1,24 @@
+const readline = require('readline-sync');
 
-let today = new Date();
-let day = today.getDate();
-let hours = (today.getHours() % 12 ) || 12;;
-let minutes = today.getMinutes().toString().padStart(2, '0'); 
-let sec = today.getSeconds().toString().padStart(2, '0'); 
+let side1, side2, side3;
 
-let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+do {
+  side1 = Number(readline.question("Enter value of side1: "));
+  side2 = Number(readline.question("Enter value of side2: "));
+  side3 = Number(readline.question("Enter value of side3: "));
 
-console.log(`Today's Day - ${days[day]}`)
+  if (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)) {
+    console.log("Invalid triangle. The sum of any two sides must be greater than the third side. Please try again.");
+  }
+} while (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2));
 
-let AM_PM = today.getHours() < 12 ? "AM" : "PM";
+let semiPerimeter = (side1 + side2 + side3) / 2;
 
-console.log(`Current Time - ${hours} : ${minutes} : ${sec} ${AM_PM}`);
+let area = Math.sqrt(
+  semiPerimeter *
+  (semiPerimeter - side1) *
+  (semiPerimeter - side2) *
+  (semiPerimeter - side3)
+);
+
+console.log(`Area of Triangle using Heron's formula: ${area.toFixed(2)} square units`);
--- README.md
+++ README.md
@@ -30,12 +30,41 @@ Today's Day - Friday
 Current Time - 7 : 01 : 53 PM
 ```
 
-1. Write a JavaScript program to display the current day and time in the following format.  
-Sample Output : Today is : Tuesday.
+2. Write a JavaScript program to find the area of a triangle where three sides are given.  
+
 
 ```
+const readline = require('readline-sync');
+
+let side1, side2, side3;
+
+do {
+  side1 = Number(readline.question("Enter value of side1: "));
+  side2 = Number(readline.question("Enter value of side2: "));
+  side3 = Number(readline.question("Enter value of side3: "));
+
+  if (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)) {
+    console.log("Invalid triangle. The sum of any two sides must be greater than the third side. Please try again.");
+  }
+} while (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2));
+
+let semiPerimeter = (side1 + side2 + side3) / 2;
+
+let area = Math.sqrt(
+  semiPerimeter *
+  (semiPerimeter - side1) *
+  (semiPerimeter - side2) *
+  (semiPerimeter - side3)
+);
+
+console.log(`Area of Triangle using Heron's formula: ${area.toFixed(2)} square units`);
 
 ```
 
 ### Output
->
+```
+Enter value of side1: 23
+Enter value of side2: 20
+Enter value of side3: 27
+Area of Triangle using Heron's formula: 224.50 square units
+```
