51864411b70086cb6f58a2745370f49d09c03239
labhk <labh.k2003@gmail.com>
2023-06-05 10:24:07 +0530

    update

--- Basics/index.js
+++ Basics/index.js
@@ -1,24 +1,20 @@
 const readline = require('readline-sync');
 
-let side1, side2, side3;
+let start, end;
 
-do {
-  side1 = Number(readline.question("Enter value of side1: "));
-  side2 = Number(readline.question("Enter value of side2: "));
-  side3 = Number(readline.question("Enter value of side3: "));
+start = Number(readline.question("Enter start of year range : "))
+end = Number(readline.question("Enter end of year range : "))
 
-  if (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)) {
-    console.log("Invalid triangle. The sum of any two sides must be greater than the third side. Please try again.");
-  }
-} while (!(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2));
+for(let year = start; year <= end; year++ ){
+    let isLeapYear = false
 
-let semiPerimeter = (side1 + side2 + side3) / 2;
+    if (year % 4 === 0){
+      if (year % 100 !== 0 || year % 400 === 0 ){
+          isLeapYear = true
+      }
+    }
 
-let area = Math.sqrt(
-  semiPerimeter *
-  (semiPerimeter - side1) *
-  (semiPerimeter - side2) *
-  (semiPerimeter - side3)
-);
+    console.log(`${year} - ${isLeapYear ? "Leap Year" : "Not"}`)
+}
 
-console.log(`Area of Triangle using Heron's formula: ${area.toFixed(2)} square units`);
+
--- README.md
+++ README.md
@@ -30,6 +30,8 @@ Today's Day - Friday
 Current Time - 7 : 01 : 53 PM
 ```
 
+
+
 2. Write a JavaScript program to find the area of a triangle where three sides are given.  
 
 
@@ -67,4 +69,40 @@ Enter value of side1: 23
 Enter value of side2: 20
 Enter value of side3: 27
 Area of Triangle using Heron's formula: 224.50 square units
+```
+
+
+3. Write a JavaScript program to determine whether a year is a leap year in the Gregorian calendar for a range of years.  
+
+
+```js
+const readline = require('readline-sync');
+
+let start, end;
+start = Number(readline.question("Enter start of year range : "))
+end = Number(readline.question("Enter end of year range : "))
+
+for(let year = start; year <= end; year++ ){
+    let isLeapYear = false
+
+    if (year % 4 === 0){
+      if (year % 100 !== 0 || year % 400 === 0 ){
+          isLeapYear = true
+      }
+    }
+
+    console.log(`${year} - ${isLeapYear ? "Leap Year" : "Not"}`)
+}
+```
+
+### Ouptut
+```
+Enter start of year range : 2023
+Enter end of year range : 2028
+2023 - Not
+2024 - Leap Year
+2025 - Not
+2026 - Not
+2027 - Not
+2028 - Leap Year
 ```
